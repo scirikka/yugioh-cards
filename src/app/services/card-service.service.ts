6e18a287e0b4b392330683692685f0489838e0f9
scirikka <selim.cirikka@capgemini.com>
2021-10-29 15:42:40 +0200

    Add card-service.

---
+++ src/app/services/card-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Card } from '../entities/entities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardServiceService {
+
+  private url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+
+  constructor(private http : HttpClient) { }
+
+  getCards() : Observable<Card[]> {
+    return this.http.get<Card[]>(this.url)
+  }
+}
